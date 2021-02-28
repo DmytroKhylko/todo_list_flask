ca468302286961bf9d4b369a17a1bd861e31eebb
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-02-28 20:31:00 +0200

    task update and delete

--- src/app.py
+++ src/app.py
@@ -13,21 +13,28 @@ def before_request():
         g.user = User.from_dict(credentials[0])
 
 @app.route("/")
-def hello():
+def home():
     if 'user_id' in session:
         return render_template("home.html",name=g.user.name)
     else:
         return render_template("home.html", name=None)
 
 
-@app.route("/tasks")
+@app.route("/tasks", methods=['GET', 'POST'])
 def tasks():
+    if request.method == 'POST':
+        task = request.form['content']
+        if task != "":
+            db.addTask("INSERT INTO todolist.user_task (user_id, task) VALUES ({}, '{}');".format(session['user_id'], task))
+
+            tasks = db.sendQuery("SELECT * FROM todolist.user_task;")
+            return render_template("tasks.html", name=g.user.name, tasks=tasks)
     if 'user_id' in session:
-        tasks = db.sendQuery("SELECT * FROM todolist.tasks;")
+        tasks = db.sendQuery("SELECT * FROM todolist.user_task;")
         return render_template("tasks.html", name=g.user.name, tasks=tasks)
     else:
-        flash("Log in, please, to access your tasks", "warning")
-        return redirect(url_for("hello"))
+        flash("Log in, please, to access your tasks", "is-danger")
+        return redirect(url_for("home"))
 
 
 
@@ -44,21 +51,42 @@ def login():
                 session['user_id'] = credentials[0]['user_id']
                 return redirect(url_for("tasks"))
             else:
-                flash("Incorrect credentials!", "warning")
+                flash("Incorrect credentials!", "is-warning")
 
 
         else:
-            flash("Incorrect credentials!", "warning")
+            flash("Incorrect credentials!", "is-warning")
             return redirect(url_for("login"))
     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
     if 'user_id' in session:
-        flash("You've successfully logged out", "success")
+        flash("You've successfully logged out", "is-success")
 
     session.pop('user_id', None)
     return redirect(url_for("login"))
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    db.deleteTask(id)
+    return redirect(url_for("tasks"))
+
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    if 'user_id' in session:
+        task = db.sendQuery("SELECT * FROM todolist.user_task WHERE user_task_id = {};".format(id))
+        if request.method == 'POST':
+            updated_task = request.form['content']
+            db.updateTask(id,updated_task)
+            return redirect(url_for("tasks"))
+        else:
+            return render_template('update.html',task=task[0])
+    else:
+        flash("Log in, please, to access your tasks", "is-danger")
+        return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- src/db.py
+++ src/db.py
@@ -1,11 +1,15 @@
 import psycopg2
 import psycopg2.extras
 
+
+DB_HOST = "db"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "1234"
+
+
 def sendQuery(query):
-    DB_HOST = "db"
-    DB_NAME = "postgres"
-    DB_USER = "postgres"
-    DB_PASS = "1234"
+
 
     conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     with conn:
@@ -16,3 +20,37 @@ def sendQuery(query):
 
     conn.close()
     return result
+
+def addTask(task):
+
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    with conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+            cur.execute(task)
+
+    conn.close()   
+
+def deleteTask(id):
+
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    with conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+            cur.execute("DELETE FROM todolist.user_task WHERE user_task_id={};".format(id))
+
+    conn.close() 
+
+
+def updateTask(id, task):
+
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    with conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+
+            cur.execute("UPDATE todolist.user_task SET task='{}' WHERE user_task_id={};".format(task, id))
+
+    conn.close()   
