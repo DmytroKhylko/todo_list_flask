7c4d0332a23b9dddec6ad29b0ba52dd93d578092
Dmytro Khylko <dmytro.khylko@gmail.com>
2021-02-28 17:53:17 +0200

    Refactored routs and htmls

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, redirect, url_for, flash
+from flask import Flask, request, render_template, session, redirect, url_for, flash, g
 import db
 from models.UserModel import User
 import sys
@@ -6,26 +6,37 @@ import sys
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
+@app.before_request
+def before_request():
+    if 'user_id' in session:
+        credentials = db.sendQuery("SELECT * FROM todolist.users WHERE user_id = {};".format(session['user_id']))
+        g.user = User.from_dict(credentials[0])
+
 @app.route("/")
 def hello():
     if 'user_id' in session:
-        return redirect(url_for("tasks"))
-    return render_template("home.html",name="dmytro")
+        return render_template("home.html",name=g.user.name)
+    else:
+        return render_template("home.html", name=None)
+
 
 @app.route("/tasks")
 def tasks():
-    tasks = db.sendQuery("SELECT * FROM todolist.tasks;")
-    return render_template("tasks.html", tasks=tasks)
+    if 'user_id' in session:
+        tasks = db.sendQuery("SELECT * FROM todolist.tasks;")
+        return render_template("tasks.html", name=g.user.name, tasks=tasks)
+    else:
+        flash("Log in, please, to access your tasks", "warning")
+        return redirect(url_for("hello"))
 
 
 
-# print([x for _, x in(db.sendQuery("SELECT * FROM todolist.users WHERE user_name LIKE '{}';".format("dmytro")))[0].items()], file=sys.stdout)
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session.pop('user_id', None)
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].strip()
+        password = request.form['password'].strip()
         credentials = db.sendQuery("SELECT * FROM todolist.users WHERE user_name LIKE '{}';".format(username))
         if len(credentials) > 0:
             loginUser = User.from_dict(credentials[0])
